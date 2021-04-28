271d4f17c7a5754b4b7511b1f98d4b0a8b360574
tuckersiegel <siegel.tucker@gmail.com>
2021-04-28 00:33:08 -0400

    gtest integration

--- sail/csrc/test/test_cpp/test.cpp
+++ sail/csrc/test/test_cpp/test.cpp
@@ -4,35 +4,15 @@
 
 #include <iostream>
 
+#include <gtest/gtest.h>
+
 #define MAX_VAL 320000
 
-int main() {
-    double x[MAX_VAL];
-    double y[MAX_VAL];
-    int ndim = 1;
-    Dtype dt = Dtype::sFloat64;
-    TensorSize st = {8};
-    TensorSize sh = {MAX_VAL};
 
-    for (int i = 0; i < MAX_VAL; i++) {
-        x[i] = 2.21;
-        y[i] = 3.21;
-    }
-
-    void* xt = static_cast<void*>(x);
-    void* yt = static_cast<void*>(y);
-
-    sail::Tensor t1 = sail::Tensor(ndim, xt, dt, st, sh);
-    sail::Tensor t2 = sail::Tensor(ndim, yt, dt, st, sh);
-
-    sail::Tensor t3 = sail::ops::cast(t1, Dtype::sInt32);
-    std::cout << *(double*)(t1.data) << std::endl;
-    std::cout << *(int32_t*)(t3.data) << std::endl;
-
-    t1[0];
-
-    t1.free();
-    t2.free();
-
-    return 0;
+// Demonstrate some basic assertions.
+TEST(HelloTest, BasicAssertions) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(7 * 6, 42);
 }
--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-./build/temp.linux-x86_64-3.7/sail/csrc/test
+# ./build/temp.linux-x86_64-3.7/sail/csrc/test
+cd build/temp.linux-x86_64-3.7/sail/csrc
+ctest
