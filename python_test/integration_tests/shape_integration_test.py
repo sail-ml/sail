6b35ce4c76df85988662c3f6c687a9105affc14a
tuckersiegel <siegel.tucker@gmail.com>
2021-05-24 13:16:00 -0400

    more integration test

--- python_test/integration_tests/shape_integration_test.py
+++ python_test/integration_tests/shape_integration_test.py
@@ -25,3 +25,22 @@ def test_shape_integration_1():
 
     log_complete("SHAPE INTEGRATION 1")
 
+def test_shape_integration_2():
+    arr1 = np.random.uniform(0, 1, (12))
+
+    x1 = sail.Tensor(arr1, requires_grad=False)
+
+    arr1 = np.expand_dims(arr1, 0)
+    arr1 = np.expand_dims(arr1, 0)
+    arr1 = np.transpose(arr1, (0, 2, 1))
+    arr1 = np.squeeze(arr1, 0)
+
+    x1 = sail.expand_dims(x1, 0)
+    x1 = sail.expand_dims(x1, 0)
+    x1 = sail.transpose(x1, (0, 2, 1))
+    x1 = sail.squeeze(x1, 0)
+
+    assert_eq_np_sail(arr1, x1)
+
+    log_complete("SHAPE INTEGRATION 2")
+
