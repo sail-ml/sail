41c110b59b4fd6ad5dd3384d679e3cb407fdfc97
tuckersiegel <siegel.tucker@gmail.com>
2021-05-24 23:13:44 -0400

    add tensordot functionality

--- sail/csrc/src/ops/linalg/dots.h
+++ sail/csrc/src/ops/linalg/dots.h
@@ -13,6 +13,20 @@ namespace ops {
 Tensor matmul(const Tensor& t1, const Tensor& t2);
 Tensor tensordot(const Tensor& t1, const Tensor& t2, LongVec t1_dim,
                  LongVec t2_dim);
+Tensor tensordot(const Tensor& t1, const Tensor& t2, int axes) {
+
+    LongVec t1_dim, t2_dim;
+
+    while (axes--) {
+        t1_dim.push_back(axes);
+        t2_dim.push_back(axes);
+    }
+    std::reverse(t1_dim.begin(), t1_dim.end());
+    std::reverse(t2_dim.begin(), t2_dim.end());
+
+    return tensordot(t1, t2, t1_dim, t2_dim);
+
+}
 
 }  // namespace ops
 
