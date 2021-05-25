9478f25520b0919d6f93af27ecf5418838d786df
tuckersiegel <siegel.tucker@gmail.com>
2021-05-25 00:16:11 -0400

    remove mkl

--- builder/install.sh
+++ builder/install.sh
@@ -7,7 +7,7 @@ chmod +x ./builder/install_boost.sh
 chmod +x ./builder/install_cmake.sh
 chmod +x ./builder/install_gcc.sh
 
-./builder/install_avx2.sh
+# ./builder/install_avx2.sh
 ./builder/install_boost.sh
 ./builder/install_cmake.sh
 ./builder/install_gcc.sh
