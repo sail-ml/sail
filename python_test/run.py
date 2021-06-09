babc3d4df74ad8b3117c300b13cf68f28cb53050
tuckersiegel <siegel.tucker@gmail.com>
2021-06-09 09:27:02 -0400

    add faulthandler

--- python_test/run.py
+++ python_test/run.py
@@ -3,8 +3,10 @@ import numpy as np
 import sail
 import time
 import unittest
+import faulthandler
 
 import basic_ops_test
 
 if __name__ == "__main__":
+    faulthandler.enable()
     UnitTest.execute()
