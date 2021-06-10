5e4fb51068d99b56a0cdeacce95b39164defb519
tuckersiegel <siegel.tucker@gmail.com>
2021-06-10 18:56:59 -0400

    fix deps

--- .github/workflow_tools/install_oneapi.sh
+++ .github/workflow_tools/install_oneapi.sh
@@ -12,9 +12,17 @@ echo "deb https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sourc
 add-apt-repository "deb https://apt.repos.intel.com/oneapi all main"
 
 # apt install -y intel-oneapi-dnnl-devel intel-oneapi-mkl intel-oneapi-mpi-devel
-apt install -y intel-oneapi-dnnl-devel intel-oneapi-mkl intel-oneapi-mkl-common-devel-2021.2.0 intel-oneapi-mpi-devel
-# apt install -y intel-oneapi-mkl-common-devel-2021.2.0 
+apt install -y intel-oneapi-dnnl-devel 
+apt install -y intel-oneapi-mkl intel-oneapi-mkl-devel intel-oneapi-mkl-common-2021.2.0 intel-oneapi-mkl-common-devel-2021.2.0 
+apt install -y intel-oneapi-mpi-devel intel-oneapi-openmp
+# apt install -y intel-basekit intel-oneapi-vtune*- intel-oneapi-python-
+
+# wget https://registrationcenter-download.intel.com/akdlm/irc_nas/17769/l_BaseKit_p_2021.2.0.2883_offline.sh
+
+# bash l_BaseKit_p_2021.2.0.2883_offline.sh
+
 apt-get install libatlas-base-dev liblapack-dev libblas-dev
+
 
 # Cleanup package manager
 # apt-get autoclean && apt-get clean
